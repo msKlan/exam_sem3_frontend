ec894ccf3fc5bc0c939903527b8bdabe353d2899
Klan <michaelsanderklan@gmail.com>
2020-01-15 21:07:10 +0100

    Updates

---
+++ precache-manifest.cb44961155512695f1e133c08039ee36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520ea8b6bc614e3e642eac78c06ff8b2",
+    "url": "/exam_sem3_frontend/index.html"
+  },
+  {
+    "revision": "ddc765db940bd927a911",
+    "url": "/exam_sem3_frontend/static/css/main.791c8edd.chunk.css"
+  },
+  {
+    "revision": "7ee1fcb938f0c9c441a1",
+    "url": "/exam_sem3_frontend/static/js/2.5db37445.chunk.js"
+  },
+  {
+    "revision": "ddc765db940bd927a911",
+    "url": "/exam_sem3_frontend/static/js/main.14ba93b5.chunk.js"
+  },
+  {
+    "revision": "8f18240e532c881910f3",
+    "url": "/exam_sem3_frontend/static/js/runtime-main.95421a90.js"
+  }
+]);
--- src/settings.js
+++
@@ -1,3 +0,0 @@
-// const URL = 'http://localhost:8080/exam_sem3/api';
-const URL = 'https://aieou.dk/exam_sem3/api';
-export default URL;
